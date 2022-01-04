417e1dbd360e503b4b9900fb094a00ba1671cd86
mikebita23 <vscode://vscode.github-authentication/did-authenticate?windowid=1&code=25eaca0b24e7c00ae1e0&state=3f673413-5f64-47a8-817a-cbf1c9393c90>
2022-01-04 22:38:33 +0100

    Front
    Front

--- esnybb_magasin/src/Controller/IndexController.php
+++ esnybb_magasin/src/Controller/IndexController.php
@@ -2,14 +2,24 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController{
 
+    /**
+     * @Route("/index")
+     */
     public function hello(){
         #echo "Esnybb Magasin";
         return new Response("Bonjour bienvenu chez Esnybb Magasin");
     }
 
+
+
+
+    /**
+     * @Route("/index/{name}")
+     */
     public function helloName ($name): Response{
         return new Response ("Hello ".$name);
     } 
