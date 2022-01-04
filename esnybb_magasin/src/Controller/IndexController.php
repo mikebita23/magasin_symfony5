66aed5cdcb1a46849593c44b53c3764903c5683e
mikebita23 <vscode://vscode.github-authentication/did-authenticate?windowid=1&code=25eaca0b24e7c00ae1e0&state=3f673413-5f64-47a8-817a-cbf1c9393c90>
2022-01-04 02:59:40 +0100

    Modif controller
    Modif controller

--- esnybb_magasin/src/Controller/IndexController.php
+++ esnybb_magasin/src/Controller/IndexController.php
@@ -5,10 +5,14 @@ use Symfony\Component\HttpFoundation\Response;
 
 class IndexController{
 
-    public function Hello(){
+    public function hello(){
         #echo "Esnybb Magasin";
         return new Response("Bonjour bienvenu chez Esnybb Magasin");
     }
+
+    public function helloName ($name): Response{
+        return new Response ("Hello ".$name);
+    } 
 }
 
 
